d72c4129a0767206942463590bc2296132d5fef0
Juma Allan <allanjuma@gmail.com>
2019-02-14 08:50:33 +0300

    Adding the HTTP Interceptor

--- app/src/main/java/com/musicalcoder/authentication/network/APIClient.java
+++ app/src/main/java/com/musicalcoder/authentication/network/APIClient.java
@@ -1,5 +1,8 @@
 package com.musicalcoder.authentication.network;
 
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,11 +10,18 @@ public class APIClient {
 
     public static Retrofit getClient() {
 
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
         String baseUrl = "https://laicare.herokuapp.com/api/v2/laicare/";
 
         return new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
     }
 }
